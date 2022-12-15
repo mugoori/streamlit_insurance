dc9d9c8b5231d8678b677d89a803245fc39a99d9
mugoori <120348468+mugoori@users.noreply.github.com>
2022-12-15 15:36:37 +0900

    중간 세이브

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_home import run_home_app
 from app_eda import run_eda_app
 
 def main() :
-    st.title('insurance predict')
+    st.title('insurance predict app')
 
     menu = ['Home','EDA']
     choice = st.sidebar.selectbox('메뉴',menu)
