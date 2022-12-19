0c7c7c62baeb608443587279600959d114479c5d
mugoori <120348468+mugoori@users.noreply.github.com>
2022-12-19 16:30:59 +0900

    save

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import streamlit as st
 from app_home import run_home_app
-from app_eda import run_eda_app
 
 def main() :
     st.title('insurance predict app')
 
-    menu = ['Home','EDA']
+    menu = ['Home']
     choice = st.sidebar.selectbox('메뉴',menu)
     if choice == 'Home' :
         run_home_app()
-    elif choice == 'EDA' :
-        run_eda_app()
-
 
 if __name__ == '__main__' :
     main()
--- app_home.py
+++ app_home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import numpy as np
 import joblib
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import platform
+import altair as alt
+import plotly.express as px
 
 def run_home_app() :
-    st.balloons()
-    # ------- 풍선 날리기
-
 
     st.subheader('환영합니다.')
     st.subheader('좌측 사이드바에 데이터를 넣으면 보험료를 예측해줍니다.')
@@ -49,3 +51,49 @@ def run_home_app() :
    
     st.sidebar.text('당신의 예상 보험료는 {} 달러 입니다.'.format(y_pred))
     # ---------  결과값 보여주기
+
+    st.text('데이터 출처 : https://www.kaggle.com/datasets/mirichoi0218/insurance')
+
+    df = pd.read_csv('i_data/insurance.csv')
+    df = df.rename(columns={'sex':'gender'})
+    df = df.replace('female',0)
+    df = df.replace('male',1)
+    df = df.replace('yes',1)
+    df = df.replace('no',0)
+    # ----------------------------- 데이터 가공
+
+
+    show_df = st.checkbox('데이터 프레임')
+    if show_df :
+        st.dataframe(df.head(3))
+    show_df2 = st.checkbox('기초 통계')
+    if show_df2 :
+        st.dataframe(df.describe())
+    # ----------------------------  데이터 프레임 / 기초 통계 보여주기
+
+
+    st.subheader('컬럼 별 히스토그램')
+    column = df.columns[0:]
+    choice2 = st.selectbox('컬럼을 선택하세요',column)
+    my_bins = st.number_input('빈의 갯수를 입력하세요', 2, 30, value=2, step=1)
+
+    fig = plt.figure()
+    plt.hist(data=df , x= choice2, rwidth=0.8, bins=my_bins)
+    plt.title( choice2 +' histogram')
+    plt.xlabel(choice2)
+    plt.ylabel('Count')
+    st.pyplot(fig)
+    # --------------------------- 컬럼 별 히스토그램 보여주기
+
+
+    st.subheader('상관 관계')
+    selected_list = st.multiselect('상관 분석을 하고싶은 컬럼을 선택하세요',column)
+
+    if len(selected_list) >= 2 :
+
+        df_corr = df[selected_list].corr()
+
+        fig2 = plt.figure()
+        sb.heatmap(data=df_corr,annot=True, fmt='.2f',cmap='coolwarm',vmin=-1,vmax=1,linewidths=0.5)
+        st.pyplot(fig2)   
+    # -------------------------- 컬럼 별 상관관계 보여주기
