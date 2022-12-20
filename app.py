bf53c8f376650615e96e1a8491d95223b3022e5e
mugoori <120348468+mugoori@users.noreply.github.com>
2022-12-20 16:35:49 +0900

    save

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from app_home import run_home_app
+from app_eda import run_eda_app
 
 def main() :
     st.title('insurance predict app')
 
-    menu = ['Home']
+    menu = ['Home','EDA']
     choice = st.sidebar.selectbox('메뉴',menu)
     if choice == 'Home' :
         run_home_app()
+    if choice == 'EDA' :
+        run_eda_app()
 
 if __name__ == '__main__' :
     main()
+
